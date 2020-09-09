47c81b63f9e7c93bdeb4e8e74e6c4f0eb74e8065
Gerrit Jan de Bruin <gerritjan.debruin@gmail.com>
2020-09-09 10:27:06 +0200

    WIP

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-rsync -aPhi --delete /home/bruingjde/complexnetworks2020-experiment/ nieuw-engeland.nl:/volume1/backup1/liacs/complexnetworks2020-experiment/
+git status
 git add --all
-git commit -m "WIP"
-git push
+
+read -p "Commit? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  git commit -m "WIP"
+  git push
+fi
+
+
+rsync -aPhi --delete /home/bruingjde/complexnetworks2020-experiment/ nieuw-engeland.nl:/volume1/backup1/liacs/complexnetworks2020-experiment/
